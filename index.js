02b6c39d10d3dfcbefbd76d3e34b739f22c95529
Eric Bailey <eric@ericb.me>
2014-06-15 14:51:40 -0500

    no message

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //
 
 // ## Dependencies:
-// - [request](https://www.npmjs.org/package/request) -- simplify HTTP requests
 // - [express](https://www.npmjs.org/package/express) -- Sinatra inspired web
 //    development framework
 // - [morgan](https://www.npmjs.org/package/morgan) -- http request logger
@@ -14,27 +13,33 @@
 var express = require('express'),
   app = express(),
   debug = require('debug')('the-pun-machine'),
+  engines = require('consolidate'),
   morgan = require('morgan'),
-  multiparty = require('multiparty'),
-  router = express.Router(),
-  util = require('util')
-
-  app.use(express.static(__dirname + '/public'))
-  app.use(morgan('short'))
+  multiparty = require('multiparty');
 
 
-  app.use('/', router.get('/', function(req, res) {
-    res.render('index.html')
-  }))
+app.use(morgan('short'))
+app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views')
+app.engine('.html', engines.handlebars)
 
-  app.post('/', function(req, res) {
-    var form = new multiparty.Form()
 
-    form.parse(req, function(err, fields, files) {
-      res.send(fields.q[0])
-    })
+app.get('/', function(req, res) {
+  var data = {
+    title: 'The Pun Machine'
+  }
+  res.render('index.html', data)
+})
+
+app.post('/', function(req, res) {
+  var form = new multiparty.Form()
+
+  form.parse(req, function(err, fields, files) {
+    res.send(fields.q[0])
   })
+})
 
-  // Listen on port 3000 and say, "Hayyy.."
-  console.log('Hayyy..')
-  app.listen(process.env.PORT || 3000)
+
+// Listen on port 3000 and say, "Hayyy.."
+console.log('Hayyy..')
+app.listen(process.env.PORT || 3000)
